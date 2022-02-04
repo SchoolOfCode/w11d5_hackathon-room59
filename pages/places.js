1cfe6ffa897453991704e213d82864284ada8a1a
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-04 16:08:47 +0000

    cleaned up

--- pages/places.js
+++ pages/places.js
@@ -3,23 +3,22 @@ import Heading from "../Components/Heading";
 import Content from "../Components/Content";
 import { loremIpsum } from "../lib/data";
 import Image from "next/image";
-// import * as css from "../../styles/places.modules.css"
 
 const Places = () => {
   return (
     <div>
-      {/* <Image
-        src="/../public/images/photo-1524942434100-2b3f200f5b40.jpeg"
+      <Image
+        src="/../public/photo-1524942434100-2b3f200f5b40.jpeg"
         alt="picture of Palermo Cathedral"
         width={200}
         height={350}
       />
       <Image
-        src="/../public/images/photo-1561280618-d4a6f7d04e79.jpeg"
+        src="/../public/photo-1561280618-d4a6f7d04e79.jpeg"
         alt="picture of Celafu"
         width={350}
         height={250}
-      /> */}
+      />
       <Heading heading={"Places"} />
       <Content paragraph={loremIpsum} />
     </div>
