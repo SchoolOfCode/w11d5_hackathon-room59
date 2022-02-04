090972508c1796f9cc2bbb73d226c541e0abaf26
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-04 15:40:53 +0000

    styled a bit more

--- Components/Heading/index.js
+++ Components/Heading/index.js
@@ -1,7 +1,8 @@
 import React from "react";
+import * as css from "../../styles/heading.module.css";
 
 const Heading = ({ heading }) => {
-  return <h1>{heading}</h1>;
+  return <h1 className={css.heading}>{heading}</h1>;
 };
 
 export default Heading;
