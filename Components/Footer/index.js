07be8c78b02dfffaf9d7f9438f452a35de21ce44
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-04 15:37:19 +0000

    added images and description

--- Components/Footer/index.js
+++ Components/Footer/index.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import * as css from "../../styles/footer.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      this is a footer
+    <div className={css.footer}>
+      This was ideated, written, coded and created by room 59
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
