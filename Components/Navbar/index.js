c94f8b4ad7e83fbd0fdc2119f4dfee3bf7951f49
valeriocipolla <valeriocipolla92@gmail.com>
2022-02-04 14:00:10 +0000

    added link to navbar

--- Components/Navbar/index.js
+++ Components/Navbar/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>this is a navbar</div>;
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </li>
+    </ul>
+  );
 };
 
 export default Navbar;
